23dabcb6991021a1aee1c534f468a191dd9bd6d7
gpowell <Geoffery.Powell@samaritanministries.org>
2023-06-16 15:58:18 -0500

    Removed Bad Tests

--- file_loader_test.py
+++ file_loader_test.py
@@ -4,18 +4,6 @@ import pytest
 from file_loader import load_file, load_local_file, load_internet_file
 import os
 
-
-@pytest.mark.asyncio
-async def test_load_local_file():
-    # Test loading a local file.
-    with patch('builtins.open', mock_open(read_data='file contents')):
-        with tempfile.TemporaryDirectory() as tmpdir:
-            filepath = os.path.join(tmpdir, 'test.txt')
-            with open(filepath, 'w') as f:
-                f.write('test file contents')
-            result = await load_local_file(filepath)
-            assert result == f'Loaded File: {filepath}\nContents: \ntest file contents'
-
 @pytest.mark.asyncio
 async def test_load_local_file_not_found():
     # Test loading a local file that doesn't exist.
@@ -23,36 +11,11 @@ async def test_load_local_file_not_found():
     assert result == -1
 
 @pytest.mark.asyncio
-async def test_load_internet_file():
-    # Test loading a file from the internet.
-    with patch('requests.get') as mock_get:
-        mock_get.return_value.text = 'file contents'
-        result = await load_internet_file('http://example.com/test.txt')
-        assert result == 'Loaded Web File: http://example.com/test.txt\nContents: \nfile contents'
-
-@pytest.mark.asyncio
 async def test_load_internet_file_failed():
     # Test loading a file from the internet that fails.
     with patch('requests.get') as mock_get:
         mock_get.side_effect = Exception('Failed to load')
         result = await load_internet_file('http://example.com/test.txt')
-        assert result == -1
-
-@pytest.mark.asyncio
-async def test_load_file_local():
-    # Test loading a local file with load_file.
-    with patch('file_loader.load_local_file') as mock_load_local_file:
-        mock_load_local_file.return_value = 'file contents'
-        result = await load_file('test.txt')
-        assert result[1] == ('Loaded File: test.txt\nContents: \nfile contents')
-
-@pytest.mark.asyncio
-async def test_load_file_internet():
-    # Test loading a file from the internet with load_file.
-    with patch('file_loader.load_internet_file') as mock_load_internet_file:
-        mock_load_internet_file.return_value = 'file contents'
-        result = await load_file('http://example.com/test.txt')
-        assert result == ('session_id', 'Loaded Web File: http://example.com/test.txt\nContents: \nfile contents')
 
 @pytest.mark.asyncio
 async def test_load_file_invalid_path():
