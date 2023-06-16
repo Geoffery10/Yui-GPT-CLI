2f2713a039f904c681fdd72da841ae202505d174
Geoffery Powell <powellgeoffery@gmail.com>
2023-06-16 16:54:08 -0500

    Fixed Broken Main Tests

--- tests/main_test.py
+++ tests/main_test.py
@@ -4,19 +4,19 @@ from termcolor import colored
 
 class TestCodeBlock:
     @pytest.mark.asyncio
-    async def test_code_block(): # Code block contains ``` 
+    async def test_code_block(self): # Code block contains ``` 
         test_string = "```python\nprint('Hello world!')\n```"
         expected = colored("python\nprint('Hello world!')\n", "cyan")
         assert await code_block(test_string) == expected
 
     @pytest.mark.asyncio
-    async def test_code_block_no_code_block(): # Code block doesn't contain ```
+    async def test_code_block_no_code_block(self): # Code block doesn't contain ```
         test_string = "Hello world!"
         expected = "Hello world!"
         assert await code_block(test_string) == expected
 
     @pytest.mark.asyncio
-    async def test_code_block_multiple_code_blocks(): # Multiple code blocks
+    async def test_code_block_multiple_code_blocks(self): # Multiple code blocks
         test_string = "```python\nprint('Hello world!')\n```\nThis is another code block\n```python\nprint('Hello world!')\n```"
         expected = colored("python\nprint('Hello world!')\n", "cyan") + "This is another code block\n" + colored("python\nprint('Hello world!')\n", "cyan")
         assert await code_block(test_string) == expected
